f1c8fa85846fa356ae73bc32c4d312e7e5ace344
everydaytoothbrush <awesomedude162@gmail.com>
2019-01-30 01:21:59 -0500

    the power of choice

--- src/Pyramid.java
+++ src/Pyramid.java
@@ -1,7 +1,17 @@
+import java.util.Scanner;
 public class Pyramid {
     public static void main(String[] args) {
-        for(int i = 0; i < 6; i++){
+        // Upgrade the Pyramid
+        Scanner scan = new Scanner(System.in);
+        System.out.print("How large do you want the base of the pyramid? ");
+        int a = scan.nextInt();
+        for(int i = 1; i <= a; i++){
             System.out.println("*".repeat(i));
         }
+
+        /*
+        for(int i = 0; i < 6; i++){
+            System.out.println("*".repeat(i));
+        }*/
     }
 }
